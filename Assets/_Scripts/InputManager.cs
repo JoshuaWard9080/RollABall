29f048a768d1390e5e543019e89615f37e1a6d22
JoshuaWard9080 <144284996+JoshuaWard9080@users.noreply.github.com>
2025-01-28 20:23:11 -0800

    Collision Detecting for Jumping
    Added collision detecting so that the player can only jump while on the ground.

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -12,33 +12,51 @@ public class InputManager : MonoBehaviour
         this.canJump = setter;
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            setCanJump(true);
+            //Debug.Log("Set canJump to True");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            setCanJump(false);
+            //Debug.Log("Set canJump to False");
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 inputVector = Vector3.zero; // initialize input vector
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("User's Input: W");
+            //Debug.Log("User's Input: W");
             inputVector += Vector3.forward;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("User's Input: A");
+            //Debug.Log("User's Input: A");
             inputVector += Vector3.left;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("User's Input: S");
+            //Debug.Log("User's Input: S");
             inputVector += Vector3.back;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("User's Input: D");
+            //Debug.Log("User's Input: D");
             inputVector += Vector3.right;
         }
         if (Input.GetKey(KeyCode.Space) && canJump == true)
         {
-            Debug.Log("User's Input: Spacebar");
+            //Debug.Log("User's Input: Spacebar");
             inputVector += Vector3.up;
             this.canJump = false;
         }
