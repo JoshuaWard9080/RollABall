6c524fbe444c8f7642ca876ed228ef9258a75c14
JoshuaWard9080 <144284996+JoshuaWard9080@users.noreply.github.com>
2025-01-28 18:55:09 -0800

    Fixed Colliders & Added Vector2 Recognition
    Fixed an issue with the Plane's collider and added Vector2 inputs  to the BallController script so that the game recognizes inputs. Currently only logs the input and the resultant Vector.

--- Assets/Scenes/BallController.cs
+++ Assets/Scenes/BallController.cs
@@ -11,19 +11,27 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Calling the Update method");
+        //Debug.Log("Calling the Update method");
 
+
+        Vector2 inputVector = Vector2.zero; // initialize input vector
         if (Input.GetKey(KeyCode.W)) {
             Debug.Log("User's Input: W");
+            inputVector += Vector2.up;
         }
         if (Input.GetKey(KeyCode.A)){
             Debug.Log("User's Input: A");
+            inputVector += Vector2.left;
         }
         if (Input.GetKey(KeyCode.S)){
             Debug.Log("User's Input: S");
+            inputVector += Vector2.down;
         }
         if (Input.GetKey(KeyCode.D)){
             Debug.Log("User's Input: D");
+            inputVector += Vector2.right;
         }
+
+        Debug.Log("Resultant Vector: " + inputVector);
     }
 }
